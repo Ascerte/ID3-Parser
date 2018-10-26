d8a15e941f6f829d7d45c2d4fd9e5e37b76fc993
Mihai Tanasa <mt18886@essex.ac.uk>
2018-10-26 12:46:38 +0100

    Final

--- ID3 Parser/ConsoleInterface.cs
+++ ID3 Parser/ConsoleInterface.cs
@@ -9,14 +9,14 @@ namespace ID3_Parser
     class ConsoleUI
     {
         string dirPath;
-        IEnumerable<string> filesArray;
+        IEnumerable<string> filesArray; // an IEnumerable that holds the file paths of valid music files
         
-        List<TagLib.File> MusicList = new List<TagLib.File>();
+        List<TagLib.File> MusicList = new List<TagLib.File>(); //list of music files
 
         public int FileIndex = -1;
-        public string[] Command = new string[2] { "", "" };
+        public string[] Command = new string[2] { "", "" }; //array that holds the command line input
         private const int CmdMax = 12;
-        public string[,] CmdArray = new string[CmdMax,2] 
+        public string[,] CmdArray = new string[CmdMax,2]  //2d array that holds available commands and their respective description
         { 
             { "cd","" }, { "exit","" }, { "help","" }, { "clear", "" }, 
             { "list", "" },{ "select", "" }, { "tags", "" }, { "album", "" }, 
@@ -27,11 +27,11 @@ namespace ID3_Parser
         {
             Console.Write(">");
             string line = Console.ReadLine();
-            Command = line.Split(new char[] { ' ', ',', '.', '\t', ':' },2);
+            Command = line.Split(new char[] { ' ', ',', '.', '\t', ':' },2); //parses input and then splits the string in 2 parts, command and argument
 
         }
 
-        public bool CommandExists( string cmd)
+        private bool CommandExists( string cmd) //function that checks whether the command exists
         {
             for (int i = 0; i < CmdMax; i++)
                 if (CmdArray[i, 0] == cmd.ToLower())
@@ -39,7 +39,7 @@ namespace ID3_Parser
             return false;
         }
 
-        public void ExecuteCommand(string[] cmd)
+        private void ExecuteCommand(string[] cmd) //a long switch statement where we execute the command equivalent to input
         {
             switch (cmd[0].ToLower())
             {
@@ -101,7 +101,7 @@ namespace ID3_Parser
 
         }
 
-        private void SelectFile(string[] cmd)
+        private void SelectFile(string[] cmd)  // first argument represents the music files' index, if no arguments are input then prints the current index if applicable
         {
             if (MusicList.Count == 0)
             {
@@ -124,7 +124,7 @@ namespace ID3_Parser
 
             int aux;
 
-            if (Int32.TryParse(cmd[1], out aux) == false)
+            if (Int32.TryParse(cmd[1], out aux) == false)  //if the argument is not a number or is outside the range then it prints an error
             {
                 Console.WriteLine("Index is invalid");
                 return;
@@ -141,13 +141,13 @@ namespace ID3_Parser
             }
         }
 
-        public void SaveFile()
+        public void SaveFile()  //saves changes for current files
         {
             if (FileIndex >= 0)
                 MusicList.ElementAt(FileIndex).Save();
         }
 
-        public void PrintTags(TagLib.File file)
+        private void PrintTags(TagLib.File file) //prints all supported tags of the selected music file
         {
             if (FileIndex < 0)
                 Console.WriteLine("Select a file first");
@@ -162,7 +162,7 @@ namespace ID3_Parser
 
         }
 
-        public void ModifyTitle(TagLib.File file)
+        private void ModifyTitle(TagLib.File file)
         {
             if (Command[1].Length == 0)
                 Console.WriteLine("Not enough arguments for the specified command");
@@ -170,7 +170,7 @@ namespace ID3_Parser
                 file.Tag.Title = Command[1];  
         }
 
-        public void ModifyYear(TagLib.File file)
+        private void ModifyYear(TagLib.File file)
         {
             if (Command[1].Length == 0)
             {
@@ -184,7 +184,7 @@ namespace ID3_Parser
                 
         }
 
-        public void ModifyAlbum(TagLib.File file)
+        private void ModifyAlbum(TagLib.File file)
         {
             if (Command[1].Length == 0)
                 Console.WriteLine("Not enough arguments for the specified command");
@@ -192,7 +192,7 @@ namespace ID3_Parser
                 file.Tag.Album = Command[1];
         }
 
-        public void ModifyPerformers(TagLib.File file)
+        private void ModifyPerformers(TagLib.File file)
         {
             if (Command.Length < 2)
                 Console.WriteLine("Not enough arguments for the specified command");
@@ -204,7 +204,7 @@ namespace ID3_Parser
             }
         }
 
-        public void ModifyGenres(TagLib.File file)
+        private void ModifyGenres(TagLib.File file)
         {
             if (Command.Length < 0)
                 Console.WriteLine("Not enough arguments for the specified command");
@@ -216,7 +216,7 @@ namespace ID3_Parser
             }
         }
 
-        private void ListFiles()
+        private void ListFiles()// prints the list of all music files parsed
         {
             int index = 0;
 
@@ -227,25 +227,28 @@ namespace ID3_Parser
             }
         }
 
-        private void ShowHelp()
+        private void ShowHelp()  //prints all available commands and their description
         {
-            
-
             CmdArray[0, 1] = "Changes the current directory";
             CmdArray[1, 1] = "Provides information for available commands";
             CmdArray[2, 1] = "Terminates the program";
             CmdArray[3, 1] = "Clears the screen";
             CmdArray[4, 1] = "Lists the name, extension and size of music files in the current directory";
-            CmdArray[5, 1] = "Selects a specific file using an index after the base directory has been set";
-
-
+            CmdArray[5, 1] = "Selects a specific file using an index after the base directory has been set, or prints the current selected index";
+            CmdArray[6, 1] = "Prints all supported tags of the selected music file";
+            CmdArray[7, 1] = "Modifies the album of the selected music file";
+            CmdArray[8, 1] = "Modifies the title of the selected music file";
+            CmdArray[9, 1] = "Modifies the performers of the selected music file. Accepts multiple names separated by ','";
+            CmdArray[10, 1] = "Modifies the genres of the selected music file. Accepts multiple names separated by ','";
+            CmdArray[11, 1] = "Modifies the year of the selected music file. Accepts only positive numbers";
+         
             for (int i = 0; i < CmdMax; i++)
             {
                 Console.WriteLine("{0}{1}",CmdArray[i,0].PadRight(8), CmdArray[i, 1]) ;
             }
         }
 
-        private void SetDirectory(string[] CmdArg)
+        private void SetDirectory(string[] CmdArg) //changes the current directory or prints the currently selected directory
         {
             if (CmdArg.GetLength(0) == 1)
             {
@@ -264,7 +267,8 @@ namespace ID3_Parser
             else
             {
                 dirPath = CmdArg[1];
-                filesArray = Directory.GetFiles(dirPath, "*.*",SearchOption.AllDirectories).Where(s => s.EndsWith(".mp3") || s.EndsWith(".flac") || s.EndsWith(".m4a") || s.EndsWith(".wma"));
+                filesArray = Directory.GetFiles(dirPath, "*.*",SearchOption.AllDirectories).Where(
+                s => s.EndsWith(".mp3") || s.EndsWith(".flac") || s.EndsWith(".m4a") || s.EndsWith(".wma"));  //we are using a linq query to obtain the path of supported music files
                 Console.WriteLine("The current path is now {0}", dirPath);
                 CreateMusicList();
             }
@@ -294,7 +298,7 @@ namespace ID3_Parser
 
         public void ValidateCommand()
         {
-            Regex CommandCheck = new Regex(@"[a-zA-z0-9]+");
+            Regex CommandCheck = new Regex(@"[a-zA-z0-9]+"); // directory path must be of the format : 'X:\directory_name'
             if (!CommandCheck.IsMatch(Command[0]) || CommandExists(Command[0]) == false)
             {
                 Console.WriteLine("Error, '{0}' is not a valid command.", Command[0]);
@@ -303,21 +307,18 @@ namespace ID3_Parser
             ExecuteCommand(Command);
         }
 
-        public TagLib.File CreateFile( string path)
+        private TagLib.File CreateFile( string path) //takes a string containing a file path as parameter then creates a new music file object
         {
             TagLib.File MusicFile = TagLib.File.Create(path);
             return MusicFile;
         }
 
-        public void CreateMusicList()
+        private void CreateMusicList()  //populates list with the parsed music files
         {
             foreach(var file in filesArray)
             {
                 MusicList.Add(CreateFile(file));
-
-
             }
         }
-        
     }
 }
